e4b42203fd66b2c06e55d8656fb3152bd90612df
shehzad ali <sa8800338@gmail.com>
2024-06-08 17:36:59 -0700

    short circuit

---
+++ app.js
@@ -0,0 +1,57 @@
+// 1.let exp = "faraz" || (true + false) + 5 && " " || "true";
+    //    faraz
+
+
+
+
+// let b = 3;
+
+// 2.let exp1 = (1 + 2) + ++b || 5 && 0;
+//        7
+
+
+// let a = 5;
+
+// let exp2 = 32 && true - ++a && " " || "true";
+
+    // empty string
+
+// let exp3 = (true + 3) * 5 && undefined || "faraz" + 5;
+
+//   faraz5
+//
+
+// let exp4 = 32 + "abc" || (true + false) + false || + 5 || "true";
+
+//  32abc
+
+
+// let exp5 = false || (true + true) + 15 && "" || 50;
+
+// 50
+
+
+// let exp6 = (true + false) + 5 || false + 8 + "abc" || "true";
+
+// 6
+
+// let exp7 = "faraz" || false + 10 || "true";
+
+//   faraz
+
+// let exp8 = 12 + (false + false) + true && null || "faraz";
+
+// faraz
+
+// let exp9 = (false + true + false + 2) || "faraz" && false || 1 + 10;
+
+// 3
+
+// let exp10 = (true + false) + 5 || false + 8 + "abc" || "true";
+
+// 6
+// 
+// let exp11 = (true + false) * 3 && "" || false + true - (12 + true + true + false);
+
+
+// -13
